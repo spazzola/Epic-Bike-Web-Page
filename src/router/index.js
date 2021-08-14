31d9957592eecf07218bc8cd6f4af5d38f5af6c1
Rafal Szczotka <rafalszczotka@wp.pl>
2021-08-14 11:11:46 +0200

    Project initialized

---
+++ src/router/index.js
@@ -0,0 +1,17 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import About from '../views/About.vue'
+
+const routes = [
+  {
+    path: '/about', alias: '/',
+    name: 'About',
+    component: About
+  }
+]
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes
+})
+
+export default router
